0a34c51a82e6b4bed197d15a359c06261579a34c
Mr-Wang-Y-P <2647135548@qq.com>
2023-09-17 15:13:10 +0800

    删除

--- server/data/getMlogin.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-// const baseUrl = 'http://127.0.0.1'
-const baseUrl = 'http://121.199.6.69'
-export const getManage = async(val) =>await axios.get(`${baseUrl}:8800/managelogin/get`,{
-    params: val
-})
